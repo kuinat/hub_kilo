38d4804eb8e8735bbf4412de61c24799eadfbe62
kuinat <kuissunathalie@gmail.comm>
2023-05-15 13:25:51 +0100

    last push to authentication

--- lib/app/modules/auth/bindings/auth_binding.dart
+++ lib/app/modules/auth/bindings/auth_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
+import '../../../providers/odoo_provider.dart';
 import '../../../services/my_auth_service.dart';
 import '../controllers/auth_controller.dart';
 
@@ -9,12 +10,12 @@ class AuthBinding extends Bindings {
     Get.lazyPut<AuthController>(
       () => AuthController(),
     );
-    // Get.lazyPut<MyAuthService>(
-    //       () => MyAuthService(),
-    // );
-    // Get.lazyPut<My>(
-    //       () => MyAuthService(),
-    // );
+    Get.lazyPut<MyAuthService>(
+          () => MyAuthService(),
+    );
+    Get.lazyPut<OdooApiClient>(
+          () => OdooApiClient(),
+    );
 
   }
 }
